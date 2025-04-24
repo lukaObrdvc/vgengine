beb61f02b52d71863cc74533e3c3b4009594d369
LukaObrdvc <obradovicc.lukaa@gmail.com>
2025-04-24 21:46:17 +0200

    beginning to refactor to use projection matrix

--- code/engine.cpp
+++ code/engine.cpp
@@ -516,9 +516,6 @@ triangle TriangleWorldToRaster(triangle tri)
     v3 B_s = V3(B.x*invB, B.y*invB, B.z);
     v3 C_s = V3(C.x*invC, C.y*invC, C.z);
     
-    s32 test1 = wnd_width;
-    s32 test2 = wnd_height;
-    
     r32 Fov = Gamestate->cameraParams.fov;
     r32 aspect_ratio = (wnd_width*1.0f)/wnd_height;
 
